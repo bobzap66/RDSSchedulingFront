bb8ab7520f2d9bef580c7e7be2fb621528ae7bb9
GTulacro <grantulacro@gmail.com>
2019-10-25 10:46:47 -0400

    added login post request, needs testing

--- MorganizeAngularApp/src/app/components/login/login.component.ts
+++ MorganizeAngularApp/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginCredentials } from 'src/app/models/loginPost';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private login:LoginService) { }
+
+  username:string = "";
+  password:string = "";
+  lc:LoginCredentials = new LoginCredentials(0, "", this.username, this.password, "", []);
+
+  onSubmitLogin(username:string, password:string)
+  {
+    this.lc.username = username;
+    this.lc.password = password;
+    this.login.sendLoginInformation(this.lc);
+   
+  }
 
   ngOnInit() {
+    
   }
 
 }
