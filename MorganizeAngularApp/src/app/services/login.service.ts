b8d51e6a3d8c0ff3d56b9f9bd1905ae35eca23fd
GTulacro <grantulacro@gmail.com>
2019-10-26 16:11:14 -0400

    Login page, working endpoint with backend and user account object creation

--- MorganizeAngularApp/src/app/services/login.service.ts
+++ MorganizeAngularApp/src/app/services/login.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LoginCredentials } from 'src/app/models/loginPost';
+import { Observable, Subscription } from 'rxjs';
+import { logging } from 'protractor';
+
+
+
+
 
 
 @Injectable({
@@ -9,22 +15,37 @@ import { LoginCredentials } from 'src/app/models/loginPost';
 export class LoginService 
 {
   login_url:string = "";
+  local_url:string = "";
   lcJson:string = "";
-
+  currentUser:LoginCredentials = new LoginCredentials(0, "", "", "", "", []);
   constructor(private http:HttpClient) 
   {
     this.login_url = "http://ec2-52-202-225-1.compute-1.amazonaws.com:9999/login";
+    this.local_url = "http://localhost:9999/login";
    }
 
-   headers = new HttpHeaders({
-    'Content-Type': 'application/json'
-  });
+   headers = new HttpHeaders({ 'Content-Type':'application/json' });
 
-    sendLoginInformation(lc:LoginCredentials):Promise<LoginCredentials>
+    sendLoginInformation(lc:LoginCredentials, token:boolean)
     {
-       this.lcJson = JSON.stringify(lc);
-       let response:Promise<LoginCredentials> = this.http.post<LoginCredentials>(this.login_url, this.lcJson, {headers:this.headers}).toPromise();
-       return response;
+      
+      let num:number = 0;
+         this.http.post<LoginCredentials>(this.local_url, lc, {headers:this.headers}).subscribe(
+      (response:LoginCredentials) =>
+      {
+         this.currentUser.id = response.id;
+         this.currentUser.name = response.name;
+         this.currentUser.username = response.username;
+         this.currentUser.password = response.password;
+         this.currentUser.email = response.email;
+         this.currentUser.groups = response.groups;
+         
+       });
+          console.log(this.currentUser);
+          //route somewhere passing along this.currentUser
+       
     }
+
+  
   
 }
