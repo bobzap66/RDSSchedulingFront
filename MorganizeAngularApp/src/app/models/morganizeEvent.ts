f7e0d7c159d21ee930ac9315913c2a3b9c365461
Sam Elliott <samelliott4@github,com>
2019-10-29 12:36:58 -0400

    added event model

---
+++ MorganizeAngularApp/src/app/models/morganizeEvent.ts
@@ -0,0 +1,24 @@
+export class MorganizeEvent{
+  
+  id:number;
+  name:string;
+  startDate:number;
+  endDate:number;
+  description:string;
+  maxAttendees:number;
+  organization:Object; //TODO replace with organization object
+  appointments:Object[];
+
+  admins:Object[];
+  attendees:Object[];
+
+
+  constructor(){
+
+  }
+
+  static createEvent(ev:MorganizeEvent){
+      //TODO convert from json response to js object
+  }
+
+}
