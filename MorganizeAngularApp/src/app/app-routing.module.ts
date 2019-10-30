68db85058f490eafe95a29e963c99df233936d1b
Sam Elliott <samelliott4@github,com>
2019-10-30 19:52:28 -0400

    first wave of service refactor

--- MorganizeAngularApp/src/app/app-routing.module.ts
+++ MorganizeAngularApp/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { UserPageComponent } from './components/user-page/user-page.component';
 import { CreateUserPageComponent } from './components/create-user-page/create-user-page.component';
 import { ResultsComponent } from './components/results/results.component';
+import { OrganizationPageComponent } from './components/organization-page/organization-page.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: "userPage", component:UserPageComponent},
   {path: "createUser", component:CreateUserPageComponent},
   {path: "results", component:ResultsComponent},
+  {path: "organizations/:id", component:OrganizationPageComponent},  
   {path: "**", component:LoginComponent}
 ];
 
