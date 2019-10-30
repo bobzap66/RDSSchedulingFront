7860ab3758883a61e00fa8b3a1831eb575d7ece4
bobzap66 <oskar_talgeron@yahoo.com>
2019-10-30 10:20:59 -0400

    Updated constructor for organization model and implemented remaining organization service methods

--- MorganizeAngularApp/src/app/models/organization.ts
+++ MorganizeAngularApp/src/app/models/organization.ts
@@ -1,5 +1,6 @@
 
 import { MorganizeEvent } from 'src/app/models/morganizeEvent';
+import { Membership } from 'src/app/models/membership';
 export class Organization
 {
 
@@ -7,18 +8,30 @@ export class Organization
     o_id:number;
     name:string;
     description:string;
-    members:Array<string>;
+    members:Array<Account>;
+    admins:Array<Account>;
     events:Array<MorganizeEvent>;
     tags:Array<string>;
+    memberships:Array<Membership>
 
 
-    constructor(o_id:number, name:string, description:string, members:Array<string>, events:Array<MorganizeEvent>, tags:Array<string>)
-    {
-        this.o_id = o_id;
-        this.name = name;
-        this.description = description;
-        this.members = members;
-        this.events = events;
-        this.tags = tags;
-    }
+
+
+    constructor(){};
+
+    static createOrganization(or:Organization){
+        let o = new Organization();  
+        
+        o.name = or.name;
+        o.description = or.description;
+        o.members = or.members;
+        o.events = or.events;
+        o.tags = or.tags;
+        o.memberships = or.memberships;
+    
+        o.admins = or.memberships.filter((membership)=>membership.type === "ADMIN").map((membership)=>membership.account);
+        o.members = or.memberships.filter((membership)=>membership.type === "MEMBER").map((membership)=>membership.account);
+    
+        return o;
+      }
 }
