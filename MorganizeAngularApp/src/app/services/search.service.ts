5314e259b2318c8147e2b335f5032842d7b21042
GTulacro <grantulacro@gmail.com>
2019-10-30 15:54:56 -0400

    test new method for calling endpoints

--- MorganizeAngularApp/src/app/services/search.service.ts
+++ MorganizeAngularApp/src/app/services/search.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MorganizeEvent } from 'src/app/models/morganizeEvent';
 import { DataServiceService } from 'src/app/services/data-service.service';
 import { Organization } from '../models/organization';
+import { EMPTY } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,18 +18,26 @@ export class SearchService
 
   constructor(private http:HttpClient, private transferSearch:DataServiceService) 
   {
-    this.login_url = "http://ec2-52-202-225-1.compute-1.amazonaws.com:9999/";
-    this.local_url = "http://localhost:9999/";
+    
+   
   }
 
   headers = new HttpHeaders({ 'Content-Type':'application/json' });
 
   searchEvent(input:string)
   {
+    this.login_url = "http://ec2-52-202-225-1.compute-1.amazonaws.com:9999";
+    this.local_url = "http://localhost:9999";
+    console.log(input);
+    if(input === "")
+    {
+      this.local_url += `/events`;
+    }
+    else
+    {
+       this.local_url += `/events?tag=${input}`;
+    }
     
-    
-    this.local_url += `/events?tag=${input}`;
-
     this.http.get<any[]>(this.local_url).subscribe((response) => 
     {
       this.events = response;
@@ -40,8 +49,18 @@ export class SearchService
 
   searchOrganizations(input:string)
   {
-    this.local_url += `/organizations?tag=${input}`;
-
+    this.login_url = "http://ec2-52-202-225-1.compute-1.amazonaws.com:9999";
+    this.local_url = "http://localhost:9999";
+    console.log(input);
+    if(input === "")
+    {
+      this.local_url += `/organizations`;
+    }
+    else
+    {
+       this.local_url += `/organizations?tag=${input}`;
+    }
+    
     this.http.get<any[]>(this.local_url).subscribe((response) => 
     {
       this.orgs = response;
