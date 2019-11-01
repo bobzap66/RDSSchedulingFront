a843fb20b9ecdd67c7cc32d3db80d554238bc0c4
bobzap66 <oskar_talgeron@yahoo.com>
2019-10-31 20:47:00 -0400

    Adjusted datetime pipe

--- MorganizeAngularApp/src/app/pipes/datetime.pipe.ts
+++ MorganizeAngularApp/src/app/pipes/datetime.pipe.ts
@@ -7,19 +7,16 @@ export class DatetimePipe implements PipeTransform {
 
   monthString:string[] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-  transform(value: number, ...args:string[]):string {
-    console.log(value);
+  transform(value: number):string {
+
     let dt = new Date(value);
     let output:string;
-    let format:string = args.length > 0 ? args[0] : ""; 
-    switch(format){
-    case "iso":
-      output = dt.toISOString().split(".")[0];
-      break;
-    default:
-      let month = this.monthString[dt.getMonth() - 1]
-      output = `${String(dt.getHours()).padStart(2, "0")}:${String(dt.getMinutes()).padStart(2, "0")}:${String(dt.getSeconds()).padStart(2, "0")}  ${dt.getDate()} ${month}`;
-    } 
+
+      let month = this.monthString[dt.getMonth()]
+ 
+      
+      output = `${String(dt.getHours()).padStart(2, "0")}:${String(dt.getMinutes()).padStart(2, "0")}  ${month}-${String(dt.getDate()).padStart(2, "0")}`;
+   
         
     return output;
   }
